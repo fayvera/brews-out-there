301f87df0390352670137c18f2630c94588120d9
Fay Vera <fvera.cavalcante@gmail.com>
2021-05-27 22:00:31 -0400

    adds reducer and navbar, imports route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 // import logo from './logo.svg';
 import './App.css';
 import React from 'react'
+import { Route } from 'react-router-dom'
 
 class App extends React.Component{
 
+  // NavBar and Routes
   render(){
     return(
       <div>
+        <Route path='/Breweries'>
 
+        </Route>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -2,12 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom'
+import { createStore } from 'redux';
+import userReducer from './reducers/userReducer'
+
+
+const store = createStore(userReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <Router >
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
 

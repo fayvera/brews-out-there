ced41faeab0793b86f6aaf04e8dff2eaf9afe03b
Fay Vera <fvera.cavalcante@gmail.com>
2021-05-28 00:32:27 -0400

    adds to navbar component and styling

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react'
 
 class NavBar extends Component {
+    menuItems = [
+        {
+            title: "Home",
+            url: "#",
+            cName: 'navbar-item'
+        },
+        {
+            title: "Menu",
+            url: '#',
+            cName: 'navbar-item'
+        },
+    ]
 
     render(){
         return(
             <nav className="navbar-items">
                 <h1 className="navbar-logo"></h1>
-                <div className="menu-items"></div>
+                <div className="menu-icon"></div>
+                <ul>
+                    {this.menuItems.map((item, index) => {
+                      return(
+                        <li key={index}>
+                            <a className={item.cName} href={item.url}>
+                                {item.title}    
+                            </a>
+                        </li>
+                        )
+                    })}
+                </ul>
             </nav>
         )
     }

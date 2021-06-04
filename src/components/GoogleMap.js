20b78129a16bd62ec6679985915dd6f06d7e3b58
Fay Vera <fvera.cavalcante@gmail.com>
2021-06-04 02:09:01 -0400

    centers map

--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import './search.css'
 import { GoogleMap, useLoadScript, Marker, InfoWindow} from '@react-google-maps/api'
 
 const libraries = ["places"]
 const mapContainerStyle = {
-    width: '80vw',
+    width: '94vw',
     height: '100vh',
 }
 const center ={
@@ -11,6 +12,7 @@ const center ={
     lat: 40.730610,
     lng: -73.935242
 }
+
 export default function Map(){
     const {isLoaded, loadError} = useLoadScript({
         googleMapsApiKey: process.env.REACT_APP_GOOGLE_KEY,
@@ -20,7 +22,7 @@ export default function Map(){
     if(loadError) return 'Error loading maps';
     if(!isLoaded) return "Loading Maps";
     return(
-        <div className="map">
+        <div className="map-div">
             <GoogleMap 
             mapContainerStyle={mapContainerStyle}
             zoom={10}

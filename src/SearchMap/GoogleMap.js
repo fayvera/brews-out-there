a01979d6d132042c85f013d84823d4b7b1216f83
Fay Vera <fvera.cavalcante@gmail.com>
2021-06-07 00:53:59 -0400

    styles location button

--- src/SearchMap/GoogleMap.js
+++ src/SearchMap/GoogleMap.js
@@ -4,6 +4,7 @@ import { GoogleMap, useLoadScript, Marker, InfoWindow } from '@react-google-maps
 import usePlacesAutocomplete, { getGeocode, getLatLng } from "use-places-autocomplete"
 import { Combobox, ComboboxInput, ComboboxPopover, ComboboxList, ComboboxOption} from "@reach/combobox"
 import Locate from './Locate'
+import Search from './search'
 // import "reach/comcobox/styles."
 // import '../.env'
 import { formatRelative } from "date-fns";
@@ -51,93 +52,39 @@ export default function Map(){
     if(!isLoaded) return "Loading Maps";
 
     return(
-        <div className="map-div">
+        <div className="map-wrapper">
 
             <Search panTo={panTo}/>
             <Locate panTo={panTo}/>
 
-            <GoogleMap 
-            mapContainerStyle={mapContainerStyle}
-            zoom={10}
-            center={center}
-            onLoad={onMapLoad}
-            onClick={onMapClick}
-            >
-                {markers.map((marker, index) => (
-                <Marker key={index}
-                position={{lat: marker.lat, lng: marker.lng}}
-                // icon={<i className="fas fa-beer"/>}
-                onClick={() => {
-                    setSelected(marker)
-                }}/>))}
+            <div className="map-div">
+                <GoogleMap 
+                mapContainerStyle={mapContainerStyle}
+                zoom={10}
+                center={center}
+                onLoad={onMapLoad}
+                onClick={onMapClick}
+                >
+                    {markers.map((marker, index) => (
+                    <Marker key={index}
+                    position={{lat: marker.lat, lng: marker.lng}}
+                    // icon={<i className="fas fa-beer"/>}
+                    onClick={() => {
+                        setSelected(marker)
+                    }}/>))}
 
-            {selected ? (
-            <InfoWindow position={{lat: selected.lat, lng: selected.lng}} 
-            onCloseClick ={() => {
-                setSelected(null)
-            }}>
-                <div>
-                    <h2>Here will be the title</h2>
-                    <p>Here will be the info</p>
-                </div>
-            </InfoWindow>) : null }      
-
-            </GoogleMap>
+                {selected ? (
+                <InfoWindow position={{lat: selected.lat, lng: selected.lng}} 
+                onCloseClick ={() => {
+                    setSelected(null)
+                }}>
+                    <div>
+                        <h2>Here will be the title</h2>
+                        <p>Here will be the info</p>
+                    </div>
+                </InfoWindow>) : null }      
+                </GoogleMap>
+            </div>
         </div>
-    )
-}
-
-
-
-// break into other component
-
-function Search({ panTo }){
-    const {ready, 
-        value, 
-        suggestions: {status, data}, 
-        setValue, 
-        clearSuggestions
-        } = usePlacesAutocomplete({
-        requestOptions: {
-            // change to locations near user
-            location: {lat: () => 40.730610, lng: () => -73.935242},
-            radius: 200 * 1000.
-        }
-    })
-    
-    return (
-        <div className="search-bar">
-            <Combobox 
-            onSelect={async (address) => {
-                setValue(address, false);
-                clearSuggestions()
-
-                try {
-                    const results = await getGeocode({address});
-                    const { lat, lng } = await getLatLng(results[0])
-                    panTo({ lat, lng });
-                } catch (error){
-                    console.log("error!")
-                }
-                }}
-            >
-                <ComboboxInput 
-                value={value} 
-                onChange={(e) => {
-                setValue(e.target.value)
-            }}
-            disabled={!ready}
-            placeholder="Search by Location"/>
-            <ComboboxPopover>
-                <ComboboxList>
-                    {status === "OK" && data.map(({id, description}) => (
-                    <ComboboxOption 
-                        key={id} 
-                        value={description}/>
-                        ))}
-                </ComboboxList>
-            </ComboboxPopover>
-        </Combobox>
-    </div>
     )
 }
--- src/SearchMap/search.js
+++ src/SearchMap/search.js
@@ -1,100 +1,144 @@
 import React, { Component } from 'react'
 import {fetchBreweriesType, fetchByCity} from '../actions/fetchBreweries'
-import Map from './GoogleMap'
 import { connect } from 'react-redux';
+import Map from './GoogleMap'
 import './search.css'
 import Dropdown from './Dropdown'
 import { Multiselect } from 'multiselect-react-dropdown'
+import usePlacesAutocomplete, { getGeocode, getLatLng } from "use-places-autocomplete"
+import { Combobox, ComboboxInput, ComboboxPopover, ComboboxList, ComboboxOption} from "@reach/combobox"
 
-class Search extends Component {
-    constructor(){
-        super();
-        this.state = {
-            input: '',
-            options: [{type: "Brewery"}, {type: "Pub"}]
-        }
-    }
-    // search by location/zipcode/
+// class Search extends Component {
 
-    handleSubmit = event => {
-        event.preventDefault();
-        // look through breweries
-        // fetchBreweries(event)
-        const formatInput = this.state.input.split(' ').join('_').toLocaleLowerCase() 
-        this.props.fetchByCity(formatInput)
-    }
+
+//     handleSubmit = event => {
+//         event.preventDefault();
+//         const formatInput = this.state.input.split(' ').join('_').toLocaleLowerCase() 
+//         this.props.fetchByCity(formatInput)
+//     }
     
-    handleChange = e => {
-        this.setState({
-            [e.target.name]: e.target.value
-        })
+//     handleChange = e => {
+//         this.setState({
+//             [e.target.name]: e.target.value
+//         })
+//     }
+
+//     // onSelect(selectedList, selectedItem){
+
+//     // }
+
+//     items = [
+//         {id: 1, type: "micro"},
+//         {id: 2, type: "nano"},
+//         {id: 3, type: "regional"},
+//         {id: 4, type: "brewpub"},
+//         {id: 5, type: "large"},
+//         {id: 6, type: "planning"},
+//         {id: 7, type: "bar"},
+//         {id: 8, type: "contract"},
+//         {id: 9, type: "proprietor"},
+//         {id: 10, type: "closed"}
+
+//     ]
+//     handleFilter = () => {
+//         debugger
+//         <Dropdown items={this.items}/>
+
+//         // <Multiselect 
+//         // options={this.state.options}
+//         // selectedValues={this.state.selectedValues}
+//         // onSelect={this.onSelect}
+//         // onRemove={this.onRemove}
+//         // displayValue="type"
+//         // />
+
+//         // this.setState({
+//         //     type: value
+//         // })
+//         // const filteredData = ""
+//         // if (value == ""){
+//         //     filteredData = this.props.data;
+//         // } else {
+//         //     debugger
+//         //     filteredData = this.props.data.filter(item => {
+//         //         return item.type === value;
+//         //     })
+//         // }
+//         // can search by city, postal code, state, (or type)
+//     }
+
+//     render(){
+//         return(
+//             <div>
+//                 <form onSubmit={this.handleSubmit}>
+//                     <input type="text" 
+//                     className="search-bar"
+//                     name="input"
+//                     value={this.state.input}
+//                     placeholder="Search by location"
+//                     onChange={this.handleChange}/>
+//                     <button
+//                     className="fas fa-search" 
+//                     type="submit" 
+//                     onSubmit={this.handleSubmit}/>
+//                     <i className="fas fa-ellipsis-v" onClick={this.handleFilter}></i>
+//                 </form>
+//                 {/* <Map /> */}
+//             </div>
+//         )   
+//     }
+// }
+
+
+// export default connect(null, {fetchBreweriesType, fetchByCity})(Search)
+
+export default function Search({ panTo }){
+    const {ready, 
+        value, 
+        suggestions: {status, data}, 
+        setValue, 
+        clearSuggestions
+        } = usePlacesAutocomplete({
+        requestOptions: {
+            location: {lat: () => 40.730610, lng: () => -73.935242},
+            radius: 200 * 1000.
+        }
+    })
+    
+    const handleSelect = async (address) => {
+        setValue(address, false);
+        clearSuggestions()
+
+        try {
+            const results = await getGeocode({address});
+            const { lat, lng } = await getLatLng(results[0])
+            panTo({ lat, lng });
+        } catch (error){
+            console.log("error!")
+            }
+        }
+    const handleInput = (e) => {
+        setValue(e.target.value);
     }
 
-    onSelect(selectedList, selectedItem){
-
-    }
-
-    items = [
-        {id: 1, type: "micro"},
-        {id: 2, type: "nano"},
-        {id: 3, type: "regional"},
-        {id: 4, type: "brewpub"},
-        {id: 5, type: "large"},
-        {id: 6, type: "planning"},
-        {id: 7, type: "bar"},
-        {id: 8, type: "contract"},
-        {id: 9, type: "proprietor"},
-        {id: 10, type: "closed"}
-
-    ]
-    handleFilter = () => {
-        debugger
-        <Dropdown items={this.items}/>
-
-        // <Multiselect 
-        // options={this.state.options}
-        // selectedValues={this.state.selectedValues}
-        // onSelect={this.onSelect}
-        // onRemove={this.onRemove}
-        // displayValue="type"
-        // />
-
-        // this.setState({
-        //     type: value
-        // })
-        // const filteredData = ""
-        // if (value == ""){
-        //     filteredData = this.props.data;
-        // } else {
-        //     debugger
-        //     filteredData = this.props.data.filter(item => {
-        //         return item.type === value;
-        //     })
-        // }
-        // can search by city, postal code, state, (or type)
-    }
-
-    render(){
-        return(
-            <div>
-                <form onSubmit={this.handleSubmit}>
-                    <input type="text" 
-                    className="search-bar"
-                    name="input"
-                    value={this.state.input}
-                    placeholder="Search by location"
-                    onChange={this.handleChange}/>
-                    <button
-                    className="fas fa-search" 
-                    type="submit" 
-                    onSubmit={this.handleSubmit}/>
-                    <i className="fas fa-ellipsis-v" onClick={this.handleFilter}></i>
-                </form>
-                {/* <Map /> */}
-            </div>
-        )   
-    }
+    return (
+        <div className="search-bar">
+            <Combobox onSelect={handleSelect}>
+                <ComboboxInput 
+                value={value} 
+                onChange={handleInput}
+                disabled={!ready}
+                placeholder="Search by Location"/>
+            <ComboboxPopover>
+                <ComboboxList>
+                    {status === "OK" && data.map(({id, description}) => (
+                    <ComboboxOption 
+                        key={id} 
+                        value={description}/>
+                        ))}
+                </ComboboxList>
+            </ComboboxPopover>
+        </Combobox>
+    </div>
+    )
 }
-
-
-export default connect(null, {fetchBreweriesType, fetchByCity})(Search)

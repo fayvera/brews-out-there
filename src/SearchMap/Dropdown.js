791e1dee236b2f5f8e47404661e64f6385e7e223
Fay Vera <fvera.cavalcante@gmail.com>
2021-06-07 02:54:21 -0400

    lets items be selected in dropdown

--- src/SearchMap/Dropdown.js
+++ src/SearchMap/Dropdown.js
@@ -1,60 +1,63 @@
 import React, {useState} from 'react'
 
-export default function Dropdown({title, items = [], multiSelect = false}){
+export default function Dropdown({title, items, multiSelect = false}){
     const [open, setOpen] = useState(false);
     const [selection, setSelection] = useState([]);
-    const toggle = () => setOpen(!open)
+    const toggle = () => setOpen(!open);
 
-    const handleOnClick = (item) => {
-        if(!selection.some(current => current.id == item.id)){
-            setSelection([...selection], item)
-        } else{
+
+
+    function handleOnClick(item) {
+        if (!selection.some(current => current.id === item.id)) {
+            if (!multiSelect) {
+                setSelection([item]);
+            } else if (multiSelect) {
+                setSelection([...selection, item]);
+            }
+        } else {
             let selectionAfterRemoval = selection;
             selectionAfterRemoval = selectionAfterRemoval.filter(
                 current => current.id !== item.id
             )
            setSelection([...selectionAfterRemoval])
-
         }
     }
 
-    const isItemSelected = item => {
-        if (selection.find(current => current.id == item.id)){
+    function isItemInSelection(item) {
+        if (selection.some(current => current.id === item.id)) {
             console.log("selected")
             return true
         }
         return false
     }
 
-    return(
+    return (
         <div className='dd-wrapper'>
-            <div tabIndex={0}
+            <div 
+                tabIndex={0}
                 className="dd-header"
                 role="button"
                 onKeyPress={() => toggle(!open)} 
                 onClick={() => toggle(!open)}
-                >
-                    <div className='dd-header_title'>
-                        <p className="dd-header__title--bold"> {title} </p>
-                    </div>
-                    <div className="dd-header__action">
-                        <p> {open ? "Done" : "Open"} </p>
-                    </div>
+            >
+                <div className='dd-header__title'>
+                    <p className="dd-header__title--bold">{title}</p>
+                </div>
+                <div className="dd-header__action">
+                    <p>{open ? "Done" : "Open"}</p>
+                </div>
             </div>
             {open && (
-                <ul className="dd-list"> 
-                {items.map((item, index) => (
-                    <li className="dd-list-item" key={index}>
-                        <button type="checkbox" onClick={handleOnClick}>
+              <ul className="dd-list"> 
+                {items.map(item => (
+                    <li className="dd-list-item" key={item.id}>
+                        <button type="button" onClick={() => handleOnClick(item)}>
                             <span>{item.type}</span>
-                            <span>{isItemSelected(item) && "Selected"}</span>
+                            <span>{isItemInSelection(item) && "Selected"}</span>
                         </button>
                     </li>
-                )
-                )}
-                </ul>)}
-        </div>
-
+                ))}
+              </ul>)}
+         </div>
         )
-
 }
